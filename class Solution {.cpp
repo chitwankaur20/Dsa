350c8f61bd50989f982cb72698da6c83b83c250a
chitwan <122033850+chitwankaur20@users.noreply.github.com>
2023-07-04 22:31:19 +0530

    Create class Solution {.cpp

---
+++ class Solution {.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+       if(nums.size()==1)
+       return;
+       int l = 0;
+       int r = 0;
+       while(r<nums.size()){
+           if(nums[r]!=0){
+               swap(nums[l],nums[r]);
+               l++;
+           }
+           r++;
+       }
+    }
+};
