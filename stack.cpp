844e95aa57854a360567b574bd0d648d1f7f796f
chitwan <122033850+chitwankaur20@users.noreply.github.com>
2023-07-21 23:24:29 +0530

    commit

---
+++ stack.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                stk.push(c);
+            }
+            else{
+                if(!stk.empty()){
+                if(c == ')' && stk.top() == '('){
+                stk.pop();
+                }
+                else if(c == '}' && stk.top() == '{'){
+                stk.pop();
+                }
+                else if(c == ']' && stk.top() == '['){
+                stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+                
+            }
+            
+        }
+      return stk.empty();
+    }
+    
+};
